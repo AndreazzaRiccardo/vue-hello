c4d6d7f7e1a42e8f01886d89ccc221c921846515
AndreazzaRiccardo <riccardo.andreazza90@gmail.com>
2023-11-14 10:19:18 +0100

    add: exercise correction

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
 
 const { createApp } = Vue;
 
-const app = createApp ({
+createApp ({
     data () {
         return {
             username: "",
             imgSrc: "https://images.unsplash.com/photo-1582845512747-e42001c95638?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            textColor: "color: darkred"
+            textColor: "color: darkred",
+            visibleClass: "hidden"
         };
     },
-});
+    methods: {
+        addVisible: function() {
+            this.visibleClass = "visible"
+        }
+    }
+}).mount("#app");
 
-app.mount("#app");
